8421f40020acd0a15f3d99194a5e8de74fb3fb5f
SachithUd <you@example.com>
2017-08-07 23:53:01 +0530

    changes in README.md file

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 **Instructions to run...**
 
-Run the threadpool.sh file in the bin folder.
+*Run the threadpool.sh file in the bin folder to start the server.  
+*Run the client.sh file in the bin folder to start the client.
 
 
 
---
+++ bin/client.sh
@@ -0,0 +1,5 @@
+$port= 4040
+
+javac /home/sachith/Tutorials/TCPServerProject/src/main/java/com/example/tcp/server/project/client/SimpleClient.java
+
+java /home/sachith/Tutorials/TCPServerProject/src/main/java/com/example/tcp/server/project/client/SimpleClient $port
